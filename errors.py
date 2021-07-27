c16459e4646bb09ae42bc043fc79042affb6e332
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2021-07-27 14:46:57 +0200

    Added .gitignore

--- errors.py
+++ errors.py
@@ -1,15 +1,16 @@
-class TitleTypeError(Exception):
-	"""Exception raised for errors in the input title.
+class ParameterTypeError(Exception):
+	"""Exception raised for errors in the input parameter.
 
 	Attributes:
-		title -- input title which caused the error
+		parameter -- input parameter which caused the error
 		message -- explanation of the error
 	"""
 
-	def __init__(self, title, message="Title can only be of type string!"):
-		self.title = title
-		self.message = message
+	def __init__(self, parameter, parameter_name, message="The parameter can only be of type string!"):
+		self.parameter = parameter
+		self.message = message.replace("parameter", parameter_name)
+		self.parameter_name = parameter_name
 		super().__init__(self.message)
 
 	def __str__(self):
-		return f'{self.title} -> {self.message}'
+		return f'{self.parameter} is of type {type(self.parameter).__name__}. -> {self.message}'
