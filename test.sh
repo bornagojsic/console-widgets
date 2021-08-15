f957340f3389c6334e10ac1d7065566cc8ae47e6
Borna <bornagojsic@gmail.com>
2021-08-15 15:47:55 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 jumpto () {
     label=$1
     cmd=$(sed -n "/$label:/{:a;n;p;ba};" $0 | grep -v ':$')
@@ -7,31 +8,73 @@ jumpto () {
     exit
 }
 
+
+notest () {
+	echo "The test file ($1.py) doesn't exist!"
+	jumpto end
+}
+
+
 noexpected () {
 	echo "The file with the expected result ($1.txt) doesn't exist!"
 	jumpto end
 }
 
+
 alltests () {
+	if [[ $1 == true ]]
+	then
+		flag="-c"
+	else
+		flag=""
+	fi
+
+	touch tests/tests.results
+	
 	for f in `ls tests/`
 	do
 		if [[ "$f" != "${f%.py}" ]]
 		then
-			bash $0 $f
+			bash $0 $flag $f
 		fi
 	done
 
 	echo ======== Overall results ========
 	cat  tests/tests.results
 	echo =================================
+	echo
 
 	rm tests/tests.results
 	jumpto end
 }
 
+
+passed () {
+	if [[ $1 == false ]]
+	then
+		echo "Test $test $(tput setaf 2)passed$(tput setaf 7)!"
+ 	fi
+ 	echo "Test $test $(tput setaf 2)passed$(tput setaf 7)!" >> tests/tests.results
+}
+
+
+failed () {
+	if [[ $1 == false ]]
+	then
+		echo "Test $test $(tput setaf 1)FAILED$(tput setaf 7)!"
+	fi
+	echo "Test $test $(tput setaf 1)FAILED$(tput setaf 7)!" >> tests/tests.results
+}
+
+
 onetest () {
 	test=${1%.py}
-	echo $test
+	compact=$2
+
+	if [[ ! -f tests/$test.py ]]
+	then
+		notest $test
+	fi
 
 	if [[ ! -f tests/expected/$test.txt ]]
 	then
@@ -44,44 +87,77 @@ onetest () {
 	fi
 
 	### Testing only one example
-
-	echo
-	echo Testing $test.py
-	echo =========== Expected ============
-	cat  tests/expected/$test.txt
-	echo ============ Result =============
-	STARTTIME=$(date +%s%3N)
-	python tests/$test.py < tests/input/$test.in > tests/results/$test.txt 2> tests/results/$test.stderr
-	ENDTIME=$(date +%s%3N)
-	cat  tests/results/$test.txt
-	cat  tests/results/$test.stderr
-	echo =================================
-	totalsecs=$(expr $ENDTIME - $STARTTIME)
-	echo
- 	echo "Total time: $totalsecs ms"
-	echo
+	
+	if [[ $compact == false ]]
+	then
+		echo
+		echo Testing $test.py
+		echo =========== Expected ============
+		cat  tests/expected/$test.txt
+		echo ============ Result =============
+	fi
+	
+	
+	if [[ -f tests/input/$test.in ]]
+	then
+		STARTTIME=$(date +%s%3N)
+		python tests/$test.py < tests/input/$test.in > tests/results/$test.txt 2> tests/results/$test.stderr
+		ENDTIME=$(date +%s%3N)
+	else
+		STARTTIME=$(date +%s%3N)
+		python tests/$test.py > tests/results/$test.txt 2> tests/results/$test.stderr
+		ENDTIME=$(date +%s%3N)
+	fi
+	
+	
+	if [[ $1 == false ]]
+	then
+		cat  tests/results/$test.txt
+		cat  tests/results/$test.stderr
+		echo =================================
+		totalsecs=$(expr $ENDTIME - $STARTTIME)
+		echo
+	 	echo "Total time: $totalsecs ms"
+		echo
+ 	fi
+ 	
  	if cmp --silent -- "tests/expected/$test.txt" "tests/results/$test.txt"
  	then
- 		echo "Test $test passed!"
- 		echo "Test $test passed!" >> tests/tests.results
+ 		passed $compact
  	else
- 		echo "Test $test FAILED!"
- 		echo "Test $test FAILED!" >> tests/tests.results
+ 		failed $compact
  	fi
  	echo
  	jumpto end
 }
 
-if [[ ! -f tests/tests.results ]]
-then
-    touch tests/tests.results
-fi
 
-if [[ -n "$1" ]]
+compacttest () {
+	compact=true
+	if [[ -n "$1" ]]
+	then
+		onetest $1 $compact
+	else
+		alltests $compact
+	fi
+}
+
+
+regulartest () {
+	if [[ -n "$1" ]]
+	then
+		onetest $1 false
+	else
+		alltests false
+	fi
+}
+
+
+if [[ $1 == '-c' ]]
 then
-	onetest $1
+	compacttest $2
 else
-	alltests
+	regulartest $1
 fi
 
 set +v end
